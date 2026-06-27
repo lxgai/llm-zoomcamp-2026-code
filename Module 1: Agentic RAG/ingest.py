86c84e3051855de81ad6df9124233dbf12200d6b
Lucy Gai <lucy9282013@gmail.com>
2026-06-27 08:27:54 +0000

    module 1 part 2

---
+++ Module 1: Agentic RAG/ingest.py
@@ -0,0 +1,34 @@
+# This file will handle
+# 1) data loading
+# 2) data creation
+
+import requests
+from minsearch import Index
+
+# fetch documents
+def load_faq_data():
+    docs_url = "https://datatalks.club/faq/json/courses.json"
+    response = requests.get(docs_url)
+    courses_raw = response.json()
+
+    documents = []
+    url_prefix = "https://datatalks.club/faq"
+
+    for course in courses_raw:
+        course_url = f"""{url_prefix}{course["path"]}"""
+        course_response = requests.get(course_url)
+        course_response.raise_for_status()
+        course_data = course_response.json()
+
+        documents.extend(course_data)
+
+    return documents
+
+# create the minsearch index to help rank results
+def build_index(documents):
+    index = Index(
+        text_fields=["question", "section", "answer"],
+        keyword_fields=["course"]
+    )
+    index.fit(documents)
+    return index
